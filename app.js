4107a3e4968f518135eb9b2ea55ac27ee7189683
anshulsinghschouhan4 <gb19101998@gmail.com>
2023-09-12 21:50:48 +0530

    Ignite the APP episode 2

---
+++ app.js
@@ -0,0 +1,32 @@
+const child1=React.createElement(
+    "h1",
+    {id:"child1"},
+    "Hi i am the first child"
+
+);
+const child2=React.createElement(
+    "h1",
+    {id:"child2"},
+    "Hi i am the second child"
+
+)
+
+const parent=React.createElement(
+    "div",
+    {id :"heading"},
+    [child1,child2]
+);
+const heading= React.createElement(
+    "div",
+    {id:"parent"},
+    parent
+    
+);
+const revision=React.createElement(
+    "h1",
+    {},
+    "Hi hello"
+)
+console.log(heading)
+const root=ReactDOM.createRoot(document.getElementById("root"))
+root.render(revision)
